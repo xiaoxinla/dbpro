2ff436a6a68ade210ba23773d02a2883012283c6
xiaoxinla <747393661@qq.com>
2016-11-16 01:05:42 +0800

    add upload module

--- imgsearch/urls.py
+++ imgsearch/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'test', views.test, name='test'),
     url(r'upload/$', views.upload, name='upload'),
 ]
--- imgsearch/views.py
+++ imgsearch/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
 from .forms import *
+from .query import *
 
 # Create your views here.
 def index(request):
@@ -13,5 +14,12 @@ def upload(request):
         if form.is_valid():
             pho = Photo(image=form.cleaned_data['image'])
             pho.save()
-            return HttpResponse('image upload success')
+            raw,thumb = query(pho.image)
+            rawstr = ','.join(raw)
+            thumbstr = ','.join(thumb)
+            context = {'raw':raw, 'thumb':thumb}
+            return render(request, 'gallery.html', context)
     return HttpResponse('allow only allow POST')
+
+def test(request):
+    return render(request, 'test.html')
